bbcb730ebad1a89c8d703b7f218e97491dead3bb
hady wahba <157915807+Hadywahba@users.noreply.github.com>
2026-01-13 15:48:21 +0200

    refactor(home):create separate componnents for hero-service and occasion

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,6 @@
-import { occasionCard } from '@/lib/constants/occasion-card';
 import Hero from './_components/hero';
-import OccasionCard from './_components/occasion-card';
-import { heroService } from '@/lib/constants/hero-service';
-import HeroService from './_components/hero-service';
+import OccasionSection from './_components/occasion-section';
+import HeroServiceSection from './_components/hero-service-section';
 
 export default function Home() {
   return (
@@ -11,34 +9,10 @@ export default function Home() {
       <Hero />
 
       {/* Occasions Section */}
-      <section className="grid w-full grid-cols-1 gap-6 px-10 pb-10 sm:px-20 md:grid-cols-2 lg:grid-cols-3">
-        {occasionCard.map((occasion) => (
-          <OccasionCard
-            key={occasion.id}
-            img={occasion.img}
-            alt={occasion.alt}
-            text={occasion.text}
-            badge={occasion.badge}
-            width={occasion.width}
-          />
-        ))}
-      </section>
+      <OccasionSection />
 
       {/* Hero Service */}
-      <section className="w-full px-10 pb-28 sm:px-20">
-        <div className="grid grid-cols-1 rounded-2xl bg-[#FBEAEA] dark:bg-zinc-700 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {heroService.map((service) => (
-            <HeroService
-              key={service.id}
-              img={service.img}
-              alt={service.alt}
-              text={service.text}
-              title={service.title}
-              imgDark={service.imgDark}
-            />
-          ))}
-        </div>
-      </section>
+      <HeroServiceSection />
     </main>
   );
 }
