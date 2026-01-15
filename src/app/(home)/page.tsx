e55083a7ac832ef205209d280f6fde0df6afb3fa
Ahmed Talal <ahmed.talal.egy@gmail.com>
2026-01-15 19:04:24 +0200

    Add best selling section and Most popular section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,11 @@
+import BestSellingSection from "@/components/features/best-selling-section/best-selling";
+import MostPopularSection from "@/components/features/most-popular-section/most-popular";
 
 
 export default function Home() {
 
-  return <></>;
+  return <>
+  <BestSellingSection/>
+  <MostPopularSection/>
+  </>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Sarabun, Tajawal } from "next/font/google";
+
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const sarabun = Sarabun({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-sarabun",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const tajawal = Tajawal({
+  subsets: ["arabic"],
+  weight: ["300", "400", "500", "700", "800"],
+  variable: "--font-tajawal",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${sarabun.variable} ${tajawal.variable}`}>
         {children}
       </body>
     </html>
