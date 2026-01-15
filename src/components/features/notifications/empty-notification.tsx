edd19da8b42776c0f8521af82539b39b4fa54650
khaled955 <khaledmansour104@yahoo.com>
2026-01-16 02:56:10 +0300

    feat(notifications):create-emty-notification

--- src/components/features/notifications/empty-notification.tsx
+++ src/components/features/notifications/empty-notification.tsx
@@ -1,4 +1,5 @@
 
+"use client";
 import { BellOff } from "lucide-react";
 
 export default function EmptyNotifications() {
@@ -9,7 +10,7 @@ export default function EmptyNotifications() {
       role="status"
       aria-live="polite"
       aria-label="Empty notifications"
-      className="flex flex-col items-center gap-5 text-[#71717a] dark:text-[#a1a1aa] py-10 border-t border-t-[#d4d4d8] dark:border-t-[#52525b]"
+      className="flex flex-col items-center gap-5 text-zinc-500 dark:text-zinc-400 py-10 border-t border-t-zinc-300 dark:border-t-zinc-600"
     >
       <BellOff size={50} aria-hidden="true" focusable="false" />
 
