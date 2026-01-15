7e3c268a5cd2c44a32cbfb8d56ed9863757758ae
hady wahba <157915807+Hadywahba@users.noreply.github.com>
2026-01-15 12:32:23 +0200

    feat(home):setup dark mode

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import Hero from './_components/hero';
 import OccasionSection from './_components/occasion-section';
 import HeroServiceSection from './_components/hero-service-section';
+import ThemeToggleIcon from '@/components/layout/app/theme-toggle';
 
 export default function Home() {
   return (
     <main className="mx-auto flex flex-col items-center justify-center">
+      <ThemeToggleIcon />
       {/* Hero Section */}
       <Hero />
 
