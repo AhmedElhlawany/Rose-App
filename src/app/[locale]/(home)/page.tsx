a7e327142ea81b6fc64e7144fa1f0c07fe20bbd6
khaled955 <khaledmansour104@yahoo.com>
2026-01-21 01:43:51 +0300

    feat(pagination):add pagination component

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -2,14 +2,24 @@ import About from '@/app/[locale]/(home)/_components/about';
 import Companies from '@/app/[locale]/(home)/_components/companies';
 import Gallery from '@/app/[locale]/(home)/_components/gallery';
 import TestimonialsCarousel from './_components/testimonials';
+import AppPagination from '@/components/shared/app-pagination';
 
-export default function Home() {
+export default function Home({ params, searchParams }) {
   return (
     <>
       <About />
       <Gallery />
       <TestimonialsCarousel />
       <Companies />
+      {/* Pagination  Props Add Manually For Trial */}
+      <AppPagination
+        searchParams={searchParams}
+        show={true}
+        totalPages={10}
+        currentPage={5}
+        locale="en"
+        pathname="/"
+      />
     </>
   );
 }
