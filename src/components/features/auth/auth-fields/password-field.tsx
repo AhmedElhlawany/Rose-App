25b19156aac284553ee5b46c36cf6a08ec7a961f
hady wahba <157915807+Hadywahba@users.noreply.github.com>
2026-01-21 13:46:35 +0200

    feat(auth): add password field reusable component

---
+++ src/components/features/auth/auth-fields/password-field.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { cn } from '@/lib/utility/tailwind-merge';
+import { Eye, EyeOff } from 'lucide-react';
+import { useLocale, useTranslations } from 'next-intl';
+import React, { useState } from 'react';
+import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
+interface PasswordFieldProps<
+  T extends FieldValues,
+> extends React.HTMLAttributes<HTMLInputElement> {
+  name: Path<T>;
+  register: UseFormRegister<T>;
+  //   errors: FieldErrors<T>;
+  label: string;
+  placeholder: string;
+}
+export default function PasswordField<T extends FieldValues>({
+  name,
+  register,
+  label,
+  placeholder,
+}: PasswordFieldProps<T>) {
+  // Translation
+  const t = useTranslations('Auth');
+
+  // Hook
+  const locale = useLocale();
+
+  // State
+  const [toggle, settoggle] = useState<boolean>(false);
+
+  // Function
+  const HandleToggle = () => {
+    settoggle(!toggle);
+  };
+  return (
+    <>
+      <div className="flex w-full flex-col">
+        {/* Label */}
+        <Label className="pb-1 text-sm font-medium capitalize text-zinc-800">
+          {t(label)}
+        </Label>
+
+        {/* Input */}
+        <div className="relative">
+          <Input
+            {...register(name)}
+            placeholder={placeholder}
+            type={cn(toggle ? 'password' : 'text')}
+            className="placeholder:text-zinc-400"
+          />
+          {/* Toggle Password Button */}
+          <div>
+            <Button
+              variant={'carousel'}
+              onClick={HandleToggle}
+              type="button"
+              className={cn(
+                'absolute top-1/2 -translate-y-1/2',
+                locale === 'ar' ? 'left-0' : 'right-0',
+              )}
+            >
+              {toggle ? (
+                <EyeOff className="size-5 text-zinc-400" />
+              ) : (
+                <Eye className="size-5 text-zinc-400" />
+              )}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
